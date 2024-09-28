072aa52d6a44cfa774ded64ded5ca769ba1d680e
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-09-28 19:43:52 +0200

    replaced SqlAlchemy with Peewee

---
+++ models/mappers/guild_music_information_mapper.py
@@ -0,0 +1,15 @@
+from models.guild_music_information import Guild
+from models.queue_object import QueueEntry
+from models.dtos.GuildDto import GuildDto
+from models.dtos.QueueEntryDto import QueueEntryDto
+
+def map(guild: Guild) -> GuildDto:
+    queue_entries = QueueEntry.select().where(QueueEntry.guild == guild)
+    queue_dtos = [QueueEntryDto(url=entry.url, already_played=entry.already_played) for entry in queue_entries]
+    
+    return GuildDto(
+        discord_guild_id=guild.id,
+        loop_queue=guild.loop_queue,
+        shuffle_queue=guild.shuffle_queue,
+        queue=queue_dtos
+    )
