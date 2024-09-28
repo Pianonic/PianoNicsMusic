bf6bea779215d041e0389275bc34cced4bef601a
PianoNic <79938743+Pianonic@users.noreply.github.com>
2024-09-28 19:43:52 +0200

    replaced SqlAlchemy with Peewee

--- models/guild_music_information.py
+++ models/guild_music_information.py
@@ -1,31 +1,11 @@
-from dataclasses import dataclass
-from typing import List
-from sqlalchemy import Column, Integer, Boolean
-from models.queue_object import QueueEntryDto
-from db_utils.base import Base
-from sqlalchemy.orm import relationship
+from peewee import Model, IntegerField, BooleanField
+from db_utils.db import db
 
-class Guild(Base):
-  __tablename__ = 'guilds'
+class Guild(Model):
+    id = IntegerField(primary_key=True)
+    loop_queue = BooleanField(null=False)
+    shuffle_queue = BooleanField(null=False)
 
-  id = Column(Integer, primary_key=True)
-  loop_queue = Column(Boolean, nullable=False)
-  shuffle_queue = Column(Boolean, nullable=False)
-  queue = relationship("QueueEntry", backref="guilds", cascade="all, delete-orphan", lazy="select")
-
-@dataclass
-class GuildDto:
-  discord_guild_id: int
-  loop_queue: bool
-  shuffle_queue: bool
-  queue: List[QueueEntryDto]
-
-def map(guild: Guild) -> GuildDto:
-  queue_dtos = [QueueEntryDto(url=entry.url, already_played=entry.already_played) for entry in guild.queue]
-  
-  return GuildDto(
-    discord_guild_id=guild.id,
-    loop_queue=guild.loop_queue,
-    shuffle_queue=guild.shuffle_queue,
-    queue=queue_dtos
-  )
+    class Meta:
+        database = db
+        table_name = 'guilds'
--- models/queue_object.py
+++ models/queue_object.py
@@ -1,23 +1,14 @@
-from dataclasses import dataclass
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from db_utils.base import Base
+from peewee import Model, IntegerField, CharField, BooleanField, ForeignKeyField
+from db_utils.db import db
+from models.guild_music_information import Guild
 
-class QueueEntry(Base):
-  __tablename__ = 'queue_entry'
+class QueueEntry(Model):
+    id = IntegerField(primary_key=True)
+    guild = ForeignKeyField(Guild, backref='queue', on_delete='CASCADE')
+    url = CharField(null=False)
+    already_played = BooleanField(null=False)
+    force_play = BooleanField(null=False)
 
-  id = Column(Integer, primary_key=True, autoincrement=True)
-  guild_id = Column(Integer, ForeignKey("guilds.id"), nullable=False)
-  url = Column(String, nullable=False)
-  already_played = Column(Boolean, nullable=False)
-  force_play = Column(Boolean, nullable=False)
-
-@dataclass
-class QueueEntryDto:
-  url: str
-  already_played: bool
-
-def map(queue_entry: QueueEntry) -> QueueEntryDto:
-  return QueueEntryDto(
-    url=queue_entry.url,
-    already_played=queue_entry.already_played
-  )
+    class Meta:
+        database = db
+        table_name = 'queue_entry'
