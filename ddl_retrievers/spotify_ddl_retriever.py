e3be373223d6638559bf6b3c3ce4613d7345bc45
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-03-27 21:23:22 +0100

    Refactor audio retrieval and playback logic for improved performance and normalization
    ---
    
    resolve #36

--- ddl_retrievers/spotify_ddl_retriever.py
+++ ddl_retrievers/spotify_ddl_retriever.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from spotdl import Spotdl
 import yt_dlp as youtube_dl
+from ddl_retrievers import universal_ddl_retriever
 from models.music_information import MusicInformation
 
 load_dotenv()
@@ -12,23 +13,4 @@ async def get_streaming_url(spotify_url) -> MusicInformation:
     song = spotdl.search([spotify_url])
     youtube_url = spotdl.get_download_urls(song)[0]
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'quiet': True,
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(youtube_url, download=False)
-        track_link = info_dict.get('url', '')
-        track_name = info_dict.get('title', '')
-        track_author = info_dict.get('uploader', '')
-
-        try:
-            thumbnails = info_dict.get('thumbnails', [])
-            square_thumbnails = [thumb for thumb in thumbnails if 'width' in thumb and 'height' in thumb and thumb['width'] == thumb['height']]
-            largest_square = max(square_thumbnails, key=lambda t: t['width'])
-            thumbnail_url = largest_square['url']
-        except:
-            thumbnail_url = info_dict.get('thumbnail', '')
-
-    return MusicInformation(streaming_url=track_link, song_name=track_name, author=track_author, image_url=thumbnail_url)
+    return await universal_ddl_retriever.get_streaming_url(youtube_url)
